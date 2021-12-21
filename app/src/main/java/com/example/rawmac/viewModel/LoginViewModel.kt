e754e7bad1dcc8c2cd34abc7aa6831c9eca92aa3
LijithCL <lijithcl333@gmail.com>
2021-12-21 14:11:20 +0530

    thired commit

--- app/src/main/java/com/example/rawmac/viewModel/LoginViewModel.kt
+++ app/src/main/java/com/example/rawmac/viewModel/LoginViewModel.kt
@@ -14,7 +14,7 @@ class LoginViewModel(private val loginRepository: LoginRepository) :ViewModel() 
 
     private val users = MutableLiveData<String>()
     private val model: Model
-    private val string:String="string"
+//    private val string:String="string"
 
     private val errorMessage = "Email or Password is not valid"
 
